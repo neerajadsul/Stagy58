9cb13aab6ff19d6108b4531e8a58b06a23bf9e30
neerajadsul <neeraj.adsul@gmail.com>
2023-01-22 15:16:31 +0000

    comments keycodes, mod key backspace, arrows, quotes, backtick,tilda

--- MacSplitNNfirmware/matrix_scan_keypad.py
+++ MacSplitNNfirmware/matrix_scan_keypad.py
@@ -62,20 +62,44 @@ key_sw = {
 }
 
 KEYCODES_L = (
+    # 0,1,2,3,4,5
     Keycode.ONE, Keycode.TWO, Keycode.THREE, Keycode.FOUR, Keycode.FIVE, Keycode.SIX,
+    # 6,7,8,9,10,11
     Keycode.Q, Keycode.W, Keycode.E, Keycode.R, Keycode.T, Keycode.Y,
+    # 12,13,14,15,16,17
     Keycode.ESCAPE, Keycode.A, Keycode.S, Keycode.D, Keycode.F, Keycode.G,
+    # 18,19,20,21,22,23
     Keycode.LEFT_SHIFT, Keycode.Z, Keycode.X, Keycode.C, Keycode.V, Keycode.B,
+    # 24, 25, 26 27, 28, 29
     Keycode.LEFT_CONTROL, Keycode.OPTION , Keycode.COMMAND, Keycode.SPACE, Keycode.QUOTE, Keycode.TAB,
 )
 
 KEYCODES_R = (
+    # 0, 1, 2, 3, 4, 5
     Keycode.EQUALS, Keycode.MINUS, Keycode.ZERO, Keycode.NINE, Keycode.EIGHT, Keycode.SEVEN,
+    # 6, 7, 8, 9, 10, 11
     Keycode.RIGHT_BRACKET, Keycode.LEFT_BRACKET, Keycode.P, Keycode.O, Keycode.I, Keycode.U,
+    # 12, 13, 14, 15, 16, 17
     Keycode.RETURN, Keycode.SEMICOLON, Keycode.L, Keycode.K, Keycode.J, Keycode.H,
+    # 18, 19, 20, 21, 22, 23
     Keycode.RIGHT_SHIFT, Keycode.FORWARD_SLASH, Keycode.PERIOD, Keycode.COMMA, Keycode.M, Keycode.N,
-    Keycode.GRAVE_ACCENT, Keycode.RIGHT_CONTROL, Keycode.OPTION, Keycode.COMMAND, Keycode.BACKSLASH, Keycode.BACKSPACE,
+    # 24, 25, 26, 27, 28, 29
+    Keycode.RIGHT_CONTROL, Keycode.OPTION, Keycode.COMMAND, Keycode.SPACE, Keycode.BACKSLASH, Keycode.BACKSPACE,
 )
+
+MOD_KEY_NUM = 28
+
+# H, J, K, L as Arrow Keys
+ARROW_KEYCODES = {
+    17:Keycode.LEFT_ARROW,
+    16:Keycode.DOWN_ARROW,
+    15:Keycode.UP_ARROW,
+    14:Keycode.RIGHT_ARROW,
+    11:Keycode.QUOTE,
+    10:Keycode.GRAVE_ACCENT,
+}
+
+#
 
 led = DigitalInOut(board.GP25)
 led.direction = Direction.OUTPUT
@@ -83,7 +107,8 @@ led.direction = Direction.OUTPUT
 kbd = Keyboard(usb_hid.devices)
 
 events_R = DummyEventQueue()
-
+mod_flag_L = False
+mod_flag_R = False
 # print("Welcome to Keypad")
 while True:
     event = km.events.get()
@@ -91,10 +116,18 @@ while True:
         key_id = event.key_number
         if event.pressed:
             led.value = True
-            kbd.press(KEYCODES_L[key_id])
+            if key_id == MOD_KEY_NUM:
+                mod_flag_L = True
+                print("main", mod_flag_L)
+            else:
+                kbd.press(KEYCODES_L[key_id])
         if event.released:
             led.value = False
-            kbd.release(KEYCODES_L[key_id])
+            if key_id == MOD_KEY_NUM:
+                mod_flag_L = False
+                print("main", mod_flag_L)
+            else:
+                kbd.release(KEYCODES_L[key_id])
 
         action = str(event).split(' ')[-1]
         data = f"<{LR} {key_id} {action}\n"
@@ -108,23 +141,23 @@ while True:
         if event_R:
             key_id = event_R.key_number
             if event_R.pressed:
-                kbd.press(KEYCODES_R[key_id])
+                if mod_flag_L:
+                    if key_id in ARROW_KEYCODES.keys():
+                        kbd.press(ARROW_KEYCODES[key_id])
+                else:
+                    kbd.press(KEYCODES_R[key_id])
+                    print(mod_flag_L)
             if event_R.released:
+                if key_id in ARROW_KEYCODES.keys():
+                    kbd.release(ARROW_KEYCODES[key_id])
                 kbd.release(KEYCODES_R[key_id])
+                print(mod_flag_L)
 
         bytes_read = uart.readline()
         if not bytes_read:
             continue
         else:
             received = bytes_read.decode().strip()
-            # print('Else:', received, end='')
-            # print()
-            # print('Len:', len(received))
-            # print()
+            print(received, end='')
             ev = DummyEvent(received)
             events_R.add(ev)
-
-
-
-    ## Sending the Keyboard Keycodes
-
