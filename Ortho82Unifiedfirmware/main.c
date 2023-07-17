3c0b8862c9511fe9c753a4b7f309ea833d2f163b
Neeraj Adsul <neeraj.adsul@gmail.com>
2023-07-17 09:16:15 +0100

    Simple test using Ortho62 keyboard hardware.

--- Ortho82Unifiedfirmware/main.c
+++ Ortho82Unifiedfirmware/main.c
@@ -4,7 +4,7 @@ Version : 1.0
 Date    : 15/07/2023
 Author  : Neeraj Adsul
 Company : 125 Systems
-Comments: 
+Comments:
 
 
 Chip type               : ATxmega32A4U
@@ -72,178 +72,199 @@ Data Stack size         : 1024
 #include "usb_init.h"
 
 // Declare your global variables here
+extern USB_KEYBOARD_t usb_keyboard;
+
 
 void main(void)
 {
-// Declare your local variables here
-unsigned char n;
-uint8_t state = 0;
+	// Declare your local variables here
+	unsigned char n;
+	uint8_t state = 0;
+	uint8_t rows = 0;
+	uint8_t i;
 
-// Check the reset source
-n=RST.STATUS;
-if (n & RST_PORF_bm)
-	{
-	// Power on reset
+	// Check the reset source
+	n = RST.STATUS;
+	if (n & RST_PORF_bm)
+		{
+		// Power on reset
 
-	}
-else if (n & RST_EXTRF_bm)
-	{
-	// External reset
+		}
+	else if (n & RST_EXTRF_bm)
+		{
+		// External reset
 
-	}
-else if (n & RST_BORF_bm)
-	{
-	// Brown out reset
+		}
+	else if (n & RST_BORF_bm)
+		{
+		// Brown out reset
 
-	}
-else if (n & RST_WDRF_bm)
-	{
-	// Watchdog reset
+		}
+	else if (n & RST_WDRF_bm)
+		{
+		// Watchdog reset
 
-	}
-else if (n & RST_PDIRF_bm)
-	{
-	// Program and debug interface reset
+		}
+	else if (n & RST_PDIRF_bm)
+		{
+		// Program and debug interface reset
 
-	}
-else if (n & RST_SRF_bm)
-	{
-	// Software reset
+		}
+	else if (n & RST_SRF_bm)
+		{
+		// Software reset
 
-	}
-// Clear the reset flag
-RST.STATUS=n;
+		}
+	// Clear the reset flag
+	RST.STATUS = n;
 
-// Interrupt system initialization
-// Optimize for speed
-#pragma optsize- 
-// Make sure the interrupts are disabled
+	// Interrupt system initialization
+	// Optimize for speed
+#pragma optsize-
+	// Make sure the interrupts are disabled
 #asm("cli")
-// Low level interrupt: On
-// Round-robin scheduling for low level interrupt: Off
-// Medium level interrupt: Off
-// High level interrupt: Off
-// The interrupt vectors will be placed at the start of the Application FLASH section
-n=(PMIC.CTRL & (~(PMIC_RREN_bm | PMIC_IVSEL_bm | PMIC_HILVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_LOLVLEN_bm))) |
-	PMIC_LOLVLEN_bm;
-CCP=CCP_IOREG_gc;
-PMIC.CTRL=n;
-// Set the default priority for round-robin scheduling
-PMIC.INTPRI=0x00;
-// Restore optimization for size if needed
+	// Low level interrupt: On
+	// Round-robin scheduling for low level interrupt: Off
+	// Medium level interrupt: Off
+	// High level interrupt: Off
+	// The interrupt vectors will be placed at the start of the Application FLASH section
+	n = (PMIC.CTRL & (~(PMIC_RREN_bm | PMIC_IVSEL_bm | PMIC_HILVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_LOLVLEN_bm))) |
+		PMIC_LOLVLEN_bm;
+	CCP = CCP_IOREG_gc;
+	PMIC.CTRL = n;
+	// Set the default priority for round-robin scheduling
+	PMIC.INTPRI = 0x00;
+	// Restore optimization for size if needed
 #pragma optsize_default
 
-// Watchdog timer initialization
-watchdog_init();
+	// Watchdog timer initialization
+	watchdog_init();
 
-// System clocks initialization
-system_clocks_init();
+	// System clocks initialization
+	system_clocks_init();
 
-// Event system initialization
-event_system_init();
+	// Event system initialization
+	event_system_init();
 
-// Ports initialization
-ports_init();
+	// Ports initialization
+	ports_init();
 
-// Virtual Ports initialization
-vports_init();
+	// Virtual Ports initialization
+	vports_init();
 
-// Timer/Counter TCC0 is disabled
-tc0_disable(&TCC0);
+	// Timer/Counter TCC0 is disabled
+	tc0_disable(&TCC0);
 
-// Timer/Counter TCC1 is disabled
-tc1_disable(&TCC1);
+	// Timer/Counter TCC1 is disabled
+	tc1_disable(&TCC1);
 
-// Timer/Counter TCD0 is disabled
-tc0_disable(&TCD0);
+	// Timer/Counter TCD0 is disabled
+	tc0_disable(&TCD0);
 
-// Timer/Counter TCD1 is disabled
-tc1_disable(&TCD1);
+	// Timer/Counter TCD1 is disabled
+	tc1_disable(&TCD1);
 
-// Timer/Counter TCE0 is disabled
-tc0_disable(&TCE0);
+	// Timer/Counter TCE0 is disabled
+	tc0_disable(&TCE0);
 
-// RTC initialization
-rtcxm_init();
+	// RTC initialization
+	rtcxm_init();
 
-// USARTC0 is disabled
-usart_disable(&USARTC0);
+	// USARTC0 is disabled
+	usart_disable(&USARTC0);
 
-// USARTC1 is disabled
-usart_disable(&USARTC1);
+	// USARTC1 is disabled
+	usart_disable(&USARTC1);
 
-// USARTD0 is disabled
-usart_disable(&USARTD0);
+	// USARTD0 is disabled
+	usart_disable(&USARTD0);
 
-// USARTD1 is disabled
-usart_disable(&USARTD1);
+	// USARTD1 is disabled
+	usart_disable(&USARTD1);
 
-// USARTE0 initialization
-usarte0_init();
+	// USARTE0 initialization
+	usarte0_init();
 
-// SPIC initialization
-spic_init();
+	// SPIC initialization
+	spic_init();
 
-// SPID initialization
-spid_init();
+	// SPID initialization
+	spid_init();
 
-// TWIC initialization
-twic_init();
+	// TWIC initialization
+	twic_init();
 
-// TWIE initialization
-twie_init();
+	// TWIE initialization
+	twie_init();
 
-// ADCA initialization
-adca_init();
+	// ADCA initialization
+	adca_init();
 
-// DACB is disabled
-DACB.CTRLA=0;
+	// DACB is disabled
+	DACB.CTRLA = 0;
 
-// Analog Comparator(s) for PORTA initialization
-aca_init();
+	// Analog Comparator(s) for PORTA initialization
+	aca_init();
 
-// Globally enable interrupts
+	// Globally enable interrupts
 #asm("sei")
 
-// USB Controller initialization in Full Speed, Device mode
-usb_init_device(&usb_config);
+	// USB Controller initialization in Full Speed, Device mode
+	usb_init_device(&usb_config);
 
-// Wait for the USB device to be enumerated by the host
-while (!usb_enumerated);
+	// Wait for the USB device to be enumerated by the host
+	while (!usb_enumerated);
 
-// Wait 1.5 seconds for the operating system to
-// load any drivers needed by the USB device
-delay_ms(1500);
+	// Wait 1.5 seconds for the operating system to
+	// load any drivers needed by the USB device
+	delay_ms(1500);
 
 
-while (1)
-      {
-      // Place your code here
-	  //uint8_t rows = PORTC.IN;
-	  uint8_t i = 0;
-	  uint8_t row_mask = 0x1;
-	  uint8_t ch = 0;
-	  //if (rows & 0b1) // if PE0 is high
-	  //{
-		  //state += 1;
-	  //}
-	  //if (state > 1) {
-		  //printf("PE0 pressed!");
-		  //usb_keyboard_keypress(KS_2_AT, KM_LEFT_SHIFT);
-		  //state = 0;
-	  //}
-	  
-	  //printf("Rows: ");
-	  //for (i=0;i<5;i++)
-	  //{
-		  //ch = '0' + (row_mask & rows);
-		  //row_mask <<= 1;
-		  //putchar(ch);
-	  //}
-	  printf("%d \r\n", PORTC.IN);
-	  
-	  //usb_keyboard_sendkeys();
-	  delay_ms(500);
+	while (1)
+		{
 
-      }
+		// Turn COL_1 to COL_5
+		rows = PORTC.IN & 0b00111111;
+		printf("%u \r\n", rows);
+		for (i = 0; i < 6; i++)
+			putchar(usb_keyboard.keys[i]);
+		if (rows > 0)
+			state = 1; else
+			state = 0;
+
+		if (rows == 0)
+			{
+			for (i = 0; i < 6; i++)
+				usb_keyboard.keys[i] = 0;
+			usb_keyboard.keys[0] = KM_RIGHT_ALT;
+			}
+
+		if (rows == 1) {
+			//usb_keyboard_keypress(KS_ESC, KM_LEFT_CTRL | KM_LEFT_SHIFT);
+			usb_keyboard.keys[0] = KM_RIGHT_ALT;
+		}
+		if (rows == 2) {
+			usb_keyboard.keys[0] = KM_LEFT_ALT;
+        }
+		if (rows == 4) {
+			usb_keyboard.keys[0] = KM_LEFT_SHIFT;
+        }
+		if (rows == 8) {
+			usb_keyboard.keys[0] = KM_LEFT_CTRL;
+        }
+		if (rows == 16) {
+			usb_keyboard.keys[0] = KM_LEFT_GUI;
+            usb_keyboard.keys[1] = KS_E;            
+        }
+
+
+
+
+
+
+
+		//usb_keyboard_sendkeys();
+		delay_ms(500);
+
+		}
 }
