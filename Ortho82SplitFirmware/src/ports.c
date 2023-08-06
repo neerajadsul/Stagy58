b4511acbf5ebd72f3c6cd68e3115a534c5f332b1
neerajadsul <neeraj.adsul@gmail.com>
2023-08-06 22:28:21 +0100

    Working USARTE0, system clock is still 2MHz internal.

--- Ortho82SplitFirmware/src/ports.c
+++ Ortho82SplitFirmware/src/ports.c
@@ -12,4 +12,5 @@ void ports_init()
     // Red LED PB3 as output, Left-right PB2 as input
     PORTB_DIR = 0b00001000;
     PORTB_OUT = 0x00;
+    PORTB_PIN3CTRL = 0b00011000;
 }
--- Ortho82SplitFirmware/src/system_clock.c
+++ Ortho82SplitFirmware/src/system_clock.c
@@ -10,16 +10,21 @@ void system_clock_init()
 {
     // Global interrupt disable
     cli();
+
+    // Oscillator Calibration not enabled yet
+    DFLLRC32M.CTRL = 0 << DFLL_ENABLE_bp;
+
     // 32MHz internal oscillator
-    CLK_CTRL = OSC_RC32MCREF0_bm;
-    // Prescaler: A Divide by 2, B Divide by 2, C divide by 2
-    CLK_PSCTRL = CLK_PSADIV2_bm | CLK_PSBCDIV_gm;
+    OSC_CTRL |= OSC_RC32MEN_bm;
+    // CLK_CTRL = CLK_SCLKSEL_RC32M_gc;
 
     while (OSC_STATUS & OSC_RC32MRDY_bm)
     {
         // wait until 32MHz internal oscillator is ready.
     }
     
+    // Prescaler: A Divide by 2, B Divide by 2, C divide by 2
+    CLK_PSCTRL = CLK_PSADIV_2_gc | CLK_PSBCDIV_1_2_gc;
 
     // Enable global interrupts
     sei();
