4c2d24865cceebc4d4064fa774c0d7466adb7831
neerajadsul <neeraj.adsul@gmail.com>
2023-08-07 22:02:38 +0100

    All in one file.

--- Ortho82SplitFirmware/src/usart.h
+++ Ortho82SplitFirmware/src/usart.h
@@ -1,9 +1,9 @@
 
-#ifndef USART_H
-#define USART_H
+// #ifndef USART_H
+// #define USART_H
 
-int usart_init(void);
+// int usart_init(void);
 
-uint8_t sendchar(uint8_t ch);
+// uint8_t sendchar(uint8_t ch);
 
-#endif
+// #endif
