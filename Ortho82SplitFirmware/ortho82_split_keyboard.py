39b0c2f623a84311fff8208514becfa10ac37d9b
neerajadsul <neeraj.adsul@gmail.com>
2023-08-13 14:07:45 +0100

    Correct the layout with British PC keycodes.

--- Ortho82SplitFirmware/ortho82_split_keyboard.py
+++ Ortho82SplitFirmware/ortho82_split_keyboard.py
@@ -14,8 +14,12 @@ class DummyEvent():
     """ Emulates Keymatrix event based on data on serial port"""
     def __init__(self, event_string):
         s = event_string.split(' ')
+        self.side = s[0][-1]
         self.key_number = int(s[1])
         self._pressed = s[2][:-1]
+
+    def __str__(self):
+        return f'<Event: key_number {self.key_number} {self._pressed}>'
 
     @property
     def pressed(self):
@@ -39,32 +43,24 @@ class DummyEventQueue():
         self.events.append(event)
 
 
-def process_event(event):
+def process_event(event, side):
 
     mod_key_flag = False
     fn_key_flag = False
     key_active = None
     key_id = event.key_number
-    if LR == 'L':
-        # Left half keys are documented above to maintain 1:1 mapping of physical placement
-        # [6 - (key_id%7) + 7*(key_id//7)] logic converts key_id to correct mapping
-        # This creates boundary value problem at 35 as it converts to 41, out of index
-        # Hence the value check to add 1 which makes correct mapping
-        if key_id > 34:
-            key_id += 1
-        key_id = 6 - (key_id%7) + 7*(key_id//7)
 
     if key_id == MOD_KEY_NUM:
         mod_key_flag = True
     elif key_id == FN_KEY_NUM:
         fn_key_flag = True
     else:
-        key_active = KEYBOARD_KEYCODES[LR][key_id]
+        key_active = KEYBOARD_KEYCODES[side][key_id]
 
-    action = str(event).split(' ')[-1]
-    data = f"<{LR} {key_id} {action}\n"
+    action = str(event).split(' ')[-1]     
+    data = f"<{side} {key_id} {action}\n"
     if LOG_DEBUG:
-        print(data, KEYBOARD_STR_KEYCODES[LR][key_id].split('.')[-1])
+        print(data, KEYBOARD_STR_KEYCODES[side][key_id].split('.')[-1])
     print(data, end='')
 
     return data, key_active, mod_key_flag, fn_key_flag
@@ -93,18 +89,18 @@ if LOG_DEBUG:
 
 
 KEYCODES_L = (
-    # 6, 5, 4, 3, 2, 1, 0
-    Keycode.ESCAPE, Keycode.F1, Keycode.F2, Keycode.F3, Keycode.F4, Keycode.F5, Keycode.F6,
-    # 13, 12, 11, 10, 9, 8, 7
-    Keycode.GRAVE_ACCENT, Keycode.ONE, Keycode.TWO, Keycode.THREE, Keycode.FOUR, Keycode.FIVE, Keycode.SIX,
-    # 20, 19, 18, 17, 16, 15, 14
-    Keycode.TAB, Keycode.Q, Keycode.W, Keycode.E, Keycode.R, Keycode.T, Keycode.Y,
-    # 27, 26, 25, 24, 23, 22, 21
-    Keycode.CAPS_LOCK, Keycode.A, Keycode.S, Keycode.D, Keycode.F, Keycode.G, Keycode.H,
-    # 34, 33, 32, 31, 30, 29, 28
-    Keycode.LEFT_SHIFT, Keycode.POUND, Keycode.Z, Keycode.X, Keycode.C, Keycode.V, Keycode.B,
-    # 40, 39, 38, 37, 36, 35
-    Keycode.LEFT_CONTROL, FN_KEY_NUM, MOD_KEY_NUM, Keycode.LEFT_ALT, Keycode.COMMAND, Keycode.SPACE,
+    # 0, 1, 2, 3, 4, 5, 6
+    Keycode.F6, Keycode.F5, Keycode.F4, Keycode.F3, Keycode.F2, Keycode.F1, Keycode.ESCAPE,
+    # 7, 8, 9, 10, 11, 12, 13
+    Keycode.SIX, Keycode.FIVE, Keycode.FOUR, Keycode.THREE, Keycode.TWO, Keycode.ONE, Keycode.KEYPAD_BACKSLASH ,
+    # 14, 15, 16, 17, 18, 19, # 20
+    Keycode.Y, Keycode.T, Keycode.R, Keycode.E, Keycode.W, Keycode.Q, Keycode.TAB,
+    # 21, 22, 23, 24, 25, 26, # 27
+    Keycode.H, Keycode.G, Keycode.F, Keycode.D, Keycode.S, Keycode.A, Keycode.CAPS_LOCK,
+    # 28, 29, 30, 31, 32, 33, # 34
+    Keycode.B, Keycode.V, Keycode.C, Keycode.X, Keycode.Z, Keycode.GRAVE_ACCENT, Keycode.LEFT_SHIFT,
+    # 35, 36, 37, 38, 39, # 40
+    Keycode.SPACE, Keycode.COMMAND, Keycode.LEFT_ALT, MOD_KEY_NUM, FN_KEY_NUM, Keycode.LEFT_CONTROL,
 )
 
 KEYCODES_R = (
@@ -115,7 +111,7 @@ KEYCODES_R = (
     # 14, 15, 16, 17, 18, 19, 20
     Keycode.U, Keycode.I, Keycode.O, Keycode.P, Keycode.LEFT_BRACKET, Keycode.RIGHT_BRACKET, Keycode.ENTER,
     # 21, 22, 23, 24, 25, 26, 27
-    Keycode.J, Keycode.K, Keycode.L, Keycode.SEMICOLON, Keycode.QUOTE, Keycode.BACKSLASH, Keycode.RETURN,
+    Keycode.J, Keycode.K, Keycode.L, Keycode.SEMICOLON, Keycode.QUOTE, Keycode.POUND, Keycode.RETURN,
     # 28, 29, 30, 31, 32, 33, 34
     Keycode.N, Keycode.M, Keycode.COMMA, Keycode.PERIOD, Keycode.FORWARD_SLASH, Keycode.UP_ARROW, Keycode.RIGHT_SHIFT,
     # 35, 36, 37, 38, 39, 40
@@ -123,6 +119,14 @@ KEYCODES_R = (
 )
 
 KEYBOARD_KEYCODES = {'L': KEYCODES_L, 'R': KEYCODES_R}
+
+
+STR_KEYCODES_L = ['Keycode.F6', 'Keycode.F5', 'Keycode.F4', 'Keycode.F3', 'Keycode.F2', 'Keycode.F1', 'Keycode.ESCAPE',
+'Keycode.SIX', 'Keycode.FIVE', 'Keycode.FOUR', 'Keycode.THREE', 'Keycode.TWO', 'Keycode.ONE', 'Keycode.GRAVE_ACCENT', 
+'Keycode.Y', 'Keycode.T', 'Keycode.R', 'Keycode.E', 'Keycode.W', 'Keycode.Q', 'Keycode.TAB', 
+'Keycode.H', 'Keycode.G', 'Keycode.F', 'Keycode.D', 'Keycode.S', 'Keycode.A', 'Keycode.CAPS_LOCK',
+'Keycode.B', 'Keycode.V', 'Keycode.C', 'Keycode.X', 'Keycode.Z', 'Keycode.POUND', 'Keycode.LEFT_SHIFT',
+'Keycode.SPACE', 'Keycode.COMMAND', 'Keycode.LEFT_ALT', 'MOD_KEY_NUM', 'FN_KEY_NUM', 'Keycode.LEFT_CONTROL']
 
 STR_KEYCODES_R = [
 'Keycode.F7', 'Keycode.F8', 'Keycode.F9', 'Keycode.F10', 'Keycode.F11', 'Keycode.F12', 'Keycode.DELETE,',
@@ -132,12 +136,7 @@ STR_KEYCODES_R = [
 'Keycode.N', 'Keycode.M', 'Keycode.COMMA', 'Keycode.PERIOD', 'Keycode.FORWARD_SLASH', 'Keycode.UP_ARROW', 'Keycode.RIGHT_SHIFT',
 'Keycode.SPACE', 'Keycode.RIGHT_GUI', 'Keycode.RIGHT_ALT', 'Keycode.LEFT_ARROW', 'Keycode.DOWN_ARROW', 'Keycode.RIGHT_ARROW']
 
-STR_KEYCODES_L = ['Keycode.ESCAPE', 'Keycode.F1', 'Keycode.F2', 'Keycode.F3', 'Keycode.F4', 'Keycode.F5', 'Keycode.F6,',
-'Keycode.GRAVE_ACCENT', 'Keycode.ONE', 'Keycode.TWO', 'Keycode.THREE', 'Keycode.FOUR', 'Keycode.FIVE', 'Keycode.SIX',
-'Keycode.TAB', 'Keycode.Q', 'Keycode.W', 'Keycode.E', 'Keycode.R', 'Keycode.T', 'Keycode.Y',
-'Keycode.CAPS_LOCK', 'Keycode.A', 'Keycode.S', 'Keycode.D', 'Keycode.F', 'Keycode.G', 'Keycode.H,',
-'Keycode.LEFT_SHIFT', 'Keycode.POUND', 'Keycode.Z', 'Keycode.X', 'Keycode.C', 'Keycode.V', 'Keycode.B',
-'Keycode.LEFT_CONTROL','FN_KEY', 'MOD_KEY', 'Keycode.LEFT_ALT', 'Keycode.COMMAND', 'Keycode.SPACE']
+
 
 KEYBOARD_STR_KEYCODES = {'L': STR_KEYCODES_L, 'R': STR_KEYCODES_R}
 
@@ -164,7 +163,7 @@ dummy_event_queue = DummyEventQueue()
 while True:
     event = km.events.get()
     if event:
-        data, key_active, mod_key_flag, fn_key_flag = process_event(event)
+        data, key_active, mod_key_flag, fn_key_flag = process_event(event, LR)
         if USB_connected:
             if event.pressed:
                 kbd.press(key_active)
@@ -180,13 +179,18 @@ while True:
         if len(serial_data) < 10:
             uart.reset_input_buffer()
             continue
-            
-        received_data = serial_data.decode().strip()
-        print(received_data, end='')
+
+        received_data = serial_data.decode().strip()        
         ev = DummyEvent(received_data)
         dummy_event_queue.add(ev)
 
-    event_dummy = dummy_event_queue.get()
+    event_dummy = dummy_event_queue.get()    
 
     if event_dummy:
-        data, key_active, mod_key_flag, fn_key_flag = process_event(event_dummy)
+        # print('event_dummy', event_dummy, event_dummy.side)
+        data, key_active, mod_key_flag, fn_key_flag = process_event(event_dummy, event_dummy.side)
+        if event_dummy.pressed:
+            kbd.press(key_active)
+        if event_dummy.released:
+            kbd.release(key_active)
+        # print('Dummy:', data)
